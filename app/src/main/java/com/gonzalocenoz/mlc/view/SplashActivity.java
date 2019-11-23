971e648a17463a79a127d893292b8a5e76306262
GonzaloCenoz <gonzalo.cenoz@gmail.com>
2019-11-23 14:02:06 -0300

    splash, icons, styles

---
+++ app/src/main/java/com/gonzalocenoz/mlc/view/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.gonzalocenoz.mlc.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.gonzalocenoz.mlc.view.productsSearch.ProductsSearchActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Start home activity
+        startActivity(new Intent(SplashActivity.this, ProductsSearchActivity.class));
+        // close splash activity
+        finish();
+    }
+
+}
